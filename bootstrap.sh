749aba417a730b1d20bf0fd41535eec311986c85
ryoya ito <horusu.dqx@gmail.com>
2020-03-03 22:49:08 +0900

    make functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,47 @@
-# link dotfiles
-for dotfile in `find . -maxdepth 1 | xargs basename | grep -E '^\..+'`;
-do
-    if [ $dotfile = '.git' ]  || [ $dotfile = '.gitmodules' ] || [ $dotfile = '.ruby-version' ] || [ $dotfile = '.gitignore' ]; then continue; fi
-    echo "link $(pwd)/$dotfile to ~/$dotfile"
-    ln -fns $(pwd)/$dotfile ~/$dotfile
-done
+link_dotfiles() {
+    for dotfile in `find . -maxdepth 1 | xargs basename | grep -E '^\..+'`;
+    do
+        if [ $dotfile = '.git' ]  || [ $dotfile = '.gitmodules' ] || [ $dotfile = '.ruby-version' ] || [ $dotfile = '.gitignore' ]; then continue; fi
+        echo "link $(pwd)/$dotfile to ~/$dotfile"
+        ln -fns $(pwd)/$dotfile ~/$dotfile
+    done
+}
 
-# homebrew
-if ! type "brew" > /dev/null; then
-    echo '`brew` not found. Install Homebrew'
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+homebrew() {
+    if ! type "brew" > /dev/null; then
+        echo '`brew` not found. Install Homebrew'
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
 
-brew bundle check --global
-if [ $? -ne 0 ]; then
-    echo 'Install `brew` dependencies...'
-    brew bundle --global
-else
-    echo '`brew` dependencies are satisfied :smile:' | emojify
-fi
+    brew bundle check --global
+    if [ $? -ne 0 ]; then
+        echo 'Install `brew` dependencies...'
+        brew bundle --global
+    else
+        echo '`brew` dependencies are satisfied :smile:' | emojify
+    fi
+}
 
-# macOS and Xcode setting
-if [ "$(uname)" == 'Darwin' ]; then
+setting() {
     echo 'macOS and Xcode setting...'
     ./.macos/macos.sh
     ./.xcode/xcode.sh
     ./.iterm/iterm.sh
-fi
+}
 
-# install .ruby-version's ruby if not installed
-if [ "$(rbenv versions --bare | grep $(rbenv local))" = '' ]; then
-    rbenv install
-fi
-bundle install
+ruby_bundle() {
+    # install .ruby-version's ruby if not installed
+    if [ "$(rbenv versions --bare | grep $(rbenv local))" = '' ]; then
+        rbenv install
+    fi
+    bundle install
+}
+
+main() {
+    link_dotfiles
+    homebrew
+    setting
+    ruby_bundle
+}
+
+main
