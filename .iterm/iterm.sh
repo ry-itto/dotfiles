b98e1cf1d819961b59da1361af018470cc7c7a7b
ryoya ito <horusu.dqx@gmail.com>
2020-04-04 11:41:33 +0900

    設定を特定のフォルダから読むように書き換え

--- .iterm/iterm.sh
+++ .iterm/iterm.sh
@@ -2,3 +2,4 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # iTerm 環境設定ファイルを設定
 defaults write com.googlecode.iterm2 PrefsCustomFolder $SCRIPT_DIR
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
