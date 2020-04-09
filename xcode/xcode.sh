aec4db0ba020cc2a9628549d1c5cde649d80d4df
ryoya ito <horusu.dqx@gmail.com>
2020-04-09 13:42:25 +0900

    modify

--- xcode/xcode.sh
+++ xcode/xcode.sh
@@ -1,3 +1,8 @@
+if type "xcodebuild" > /dev/null; then
+    echo 'please install xcode before executing this script.'
+    exit 1
+fi
+
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 # Xcode 上でビルド時間を表示する
@@ -7,6 +12,8 @@ defaults write com.apple.dt.Xcode ShowBuildOperationDuration YES
 cores=`system_profiler SPHardwareDataType | grep -o 'Cores:\s\d' | sed -e 's/[^0-9]//g'`
 # アプリをビルドする時に並列でビルドする
 defaults write com.apple.dt.Xcode IDEBuildOperationMaxNumberOfConcurrentCompileTasks $cores
+# Including whitespace-only lines 空白しかない行の空白を削除
+defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool true
 
 # submodule setup
 git submodule init
