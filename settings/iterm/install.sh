a6d4b7b6faec8073badc0d3d0e8896775ca7f0c4
ryoya ito <ry.itto.github@gmail.com>
2022-02-06 14:38:30 +0900

    Redefine

--- settings/iterm/install.sh
+++ settings/iterm/install.sh
@@ -1,5 +1,5 @@
-SCRIPT_DIR=$HOME/.iterm
+DOT_DIR=$HOME/.iterm
 
 # iTerm 環境設定ファイルを設定
-defaults write com.googlecode.iterm2 PrefsCustomFolder $SCRIPT_DIR
+defaults write com.googlecode.iterm2 PrefsCustomFolder $DOT_DIR
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
